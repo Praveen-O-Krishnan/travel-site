cb447d0290d615537722bac00bd8343d5083bd67
Praveen O Krishnan <praveen.krishnan@ushamartintech.com>
2018-07-13 16:56:50 +0530

    completed desktop and mobile header styles.

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,17 +1,19 @@
-var gulp = require('gulp');
-var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
-var cssvars = require('postcss-simple-vars');
-var nested = require('postcss-nested');
-var cssImport = require('postcss-import');
-var mixins = require('postcss-mixins');
+var gulp = require("gulp");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var cssvars = require("postcss-simple-vars");
+var nested = require("postcss-nested");
+var cssImport = require("postcss-import");
+var mixins = require("postcss-mixins");
+var hexrgba = require("postcss-hexrgba");
 
-gulp.task('styles', function() {
-	return gulp.src('app/assets/styles/style.css')
-		.pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
-		.on('error', function(errorInfo) {
-			console.log(errorInfo.toString());
-			this.emit('end');
-		})
-		.pipe(gulp.dest('app/temp/styles'));
+gulp.task("styles", function() {
+  return gulp
+    .src("app/assets/styles/style.css")
+    .pipe(postcss([cssImport, mixins, cssvars, nested, hexrgba, autoprefixer]))
+    .on("error", function(errorInfo) {
+      console.log(errorInfo.toString());
+      this.emit("end");
+    })
+    .pipe(gulp.dest("app/temp/styles"));
 });
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,25 +1,33 @@
-var gulp = require('gulp');
-var watch = require('gulp-watch');
-var browserSync = require('browser-sync').create();
+var gulp = require("gulp");
+var watch = require("gulp-watch");
+var browserSync = require("browser-sync").create();
 
-gulp.task('watch', function() {
-	browserSync.init({
-		notify: false,
-		server: {
-			baseDir: "app"
-		}
-	});
+gulp.task("watch", function() {
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "app"
+    }
+  });
 
-	watch('app/index.html', function() {
-		browserSync.reload();
-	});
+  watch("app/index.html", function() {
+    browserSync.reload();
+  });
 
-	watch('app/assets/styles/**/**.css', function() {
-		gulp.start('cssInject');
-	});
+  watch("app/assets/styles/**/*.css", function() {
+    gulp.start("cssInject");
+  });
+
+  watch("./app/assets/scripts/**/*.js", function() {
+    gulp.start("scriptsRefresh");
+  });
 });
 
-gulp.task('cssInject', ['styles'], function() {
-	return gulp.src('app/temp/styles/style.css')
-		.pipe(browserSync.stream());
+gulp.task("cssInject", ["styles"], function() {
+  return gulp.src("app/temp/styles/style.css").pipe(browserSync.stream());
 });
+
+gulp.task("scriptsRefresh", ["scripts"], function() {
+  browserSync.reload();
+});
+
